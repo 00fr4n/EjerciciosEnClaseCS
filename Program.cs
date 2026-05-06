71dbd73fe54b9fdc5b60ff0e52bdc980b0d9c535
00fr4n <mfpquiroz@uamv.edu.ni>
2026-05-05 22:07:03 -0600

    while1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+int numero;
+while (true)
+{
+    Console.WriteLine("Ingrese un numero entero: ");
+    if (int.TryParse(Console.ReadLine(), out numero))
+    {
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada no valida. Por favor, ingrese numero entero. ");
+        Console.ResetColor();
+    }
+}
+Console.WriteLine("Mostrando el cuadrado de los antecesores de " + numero + ":");
+int cont = 1;
+while(cont < numero)
+{
+    Console.WriteLine("El cuadrado de " + cont + " es: " + Math.Pow(cont, 2));
+    cont ++;
+}
+
